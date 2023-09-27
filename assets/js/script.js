c062e05d45299c4253040e05cd747d3d71901a21
Kevin Coppey <48347504+KevinCoppey@users.noreply.github.com>
2023-09-27 18:40:14 +0200

    add-features
    - modal for bvr added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * navbar variables
@@ -10,12 +10,13 @@ const navbar = document.querySelector("[data-navbar]");
  * element toggle function
  */
 
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
-menuToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
-
-
-
+menuToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navbar);
+});
 
 /**
  * go to top
@@ -24,11 +25,35 @@ menuToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); })
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-
   if (window.scrollY >= 800) {
     goTopBtn.classList.add("active");
   } else {
     goTopBtn.classList.remove("active");
   }
-
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "flex";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
